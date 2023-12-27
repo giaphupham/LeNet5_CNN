88ba6392ce71da7f37c5aab1624e88fffddc3db8
Phạm Trần Gia Phú <giaphupham196@gmail.com>
2023-12-27 14:59:24 +0700

    Update m2.cc

--- m2.cc
+++ m2.cc
@@ -7,15 +7,15 @@ void inference_only(int batch_size) {
   dataset.read_test_data(batch_size);
   std::cout<<"Done"<<std::endl;
   
-  // std::cout<<"Loading model...";
-  // Network dnn = createNetwork_GPU();
-  // std::cout<<"Done"<<std::endl;
+  std::cout<<"Loading model...";
+  Network dnn = createNetwork_GPU();
+  std::cout<<"Done"<<std::endl;
 
-  // dnn.forward(dataset.test_data);
-  // float acc = compute_accuracy(dnn.output(), dataset.test_labels);
-  // std::cout<<std::endl;
-  // std::cout<<"Test Accuracy: "<<acc<< std::endl;
-  // std::cout<<std::endl;
+  dnn.forward(dataset.test_data);
+  float acc = compute_accuracy(dnn.output(), dataset.test_labels);
+  std::cout<<std::endl;
+  std::cout<<"Test Accuracy: "<<acc<< std::endl;
+  std::cout<<std::endl;
 
   std::cout<<"Loading model...";
   Network dnn_2 = createNetwork_CPU(true);
